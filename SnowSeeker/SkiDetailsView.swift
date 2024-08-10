ef9778910c6af305e68fcb92132de0ee52d0528c
ayrton <ayrton.parkinson1@gmail.com>
2024-08-10 08:53:45 -0400

    Add ski and resort details

---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,35 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Ayrton Parkinson on 2024/08/10.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Elevation")
+                    .font(.caption.bold())
+                Text("\(resort.elevation)m")
+                    .font(.title3)
+            }
+            
+            VStack {
+                Text("Snow")
+                    .font(.caption.bold())
+                Text("\(resort.snowDepth)cm")
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    SkiDetailsView(resort: .example)
+}
