225e13f1bbbbc9030061a271806e0d6118026f0a
ayrton <ayrton.parkinson1@gmail.com>
2024-08-10 07:25:33 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ayrton Parkinson on 2024/08/10.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
